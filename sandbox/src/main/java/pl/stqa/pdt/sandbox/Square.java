8a69515ad558d3e336363221de32d4bace3fd51a
Asia Duda <asiaduda@gmail.com>
2018-09-06 23:02:21 +0200

    Square and retrangle areas with objects and methods

---
+++ sandbox/src/main/java/pl/stqa/pdt/sandbox/Square.java
@@ -0,0 +1,14 @@
+package pl.stqa.pdt.sandbox;
+
+public class Square {
+  public double l;
+
+  public Square (double len){
+    this.l = l;
+  }
+
+  public double area(){
+    return this.l * this.l;
+  }
+
+}
